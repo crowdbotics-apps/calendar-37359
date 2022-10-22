e54c3b9584c2eadef9198ed8c82aced85e3f6d5f
Crowdbotics <team@crowdbotics.com>
2022-10-22 09:51:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "calendar_37359",
+  url: "https://calendar_37359.botics.co",
+  api: "https://calendar_37359.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
